7ee4414704d5901db095b9f0249f4ad6f264810f
Dalbizurez <dalbizurez@correo.url.edu.gt>
2024-09-14 09:12:35 -0600

    Analisis lexico sin interfaz
    Definicion de expresiones regulares, separacion del texto en los tokens respectivos y pruebas en el archivo

---
+++ lexical.py
@@ -0,0 +1,52 @@
+import re
+
+RESERVADA = "entero|decimal|booleano|cadena|si|sino|mientras|hacer|verdadero|falso"
+OPERADOR = "\+|-|\*|\/|%|=|==|<|>|>=|<="
+SIGNO = "\(|\)|\{|\}|\"|;"
+NUMERO = r"\b\d+\b"
+IDENTIFICADOR = "[a-zA-Z](?:[a-zA-Z]|\d)*"
+
+
+def analyze(txt:str):
+    reserved = [] 
+    operator = []
+    sign = []
+    number = []
+    identifier = []
+
+    lines:list[str] = txt.splitlines()
+    for index in range(len(lines)):
+        line = lines[index]
+        for r in re.finditer(RESERVADA, line):
+            reserved.append((r.group(), index, r.start(), txt.count(r.group())))
+        for o in re.finditer(OPERADOR, line):
+            operator.append((o.group(), index, o.start(), txt.count(o.group())))
+        for s in re.finditer(SIGNO, line):
+            sign.append((s.group(), index, s.start(), txt.count(s.group())))
+        for n in re.finditer(NUMERO, line):
+            number.append((n.group(), index, n.start(), txt.count(n.group())))
+        for i in re.finditer(IDENTIFICADOR, line):
+            if not re.search(RESERVADA, i.group()):
+                identifier.append((i.group(), index, i.start(), txt.count(i.group())))
+
+
+    return reserved, operator, sign, number, identifier
+
+txt = """
+entero 45 d;
+decimal tr4 booleano;
+tr4=verdadero;
+cadena = "{45} Gabriel(Gonzales)"
+si 45 == 45 {
+    hacer falso
+}
+"""
+lines = txt.splitlines()
+for index in range(len(lines)):
+    line = lines[index]
+    matches = re.finditer(RESERVADA, line) 
+    for m in matches:
+        print(m.span(), m.group(), index)
+
+tokens = analyze(txt)
+print(tokens)
